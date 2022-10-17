b2c95122ca6d54f45a21cc2a26e25c6d788c3355
wooooozin <wooooozin@gmail.com>
2022-10-18 08:52:15 +0900

    Cell 만들고 기사 보여주기

---
+++ NewsAppUsingMVVM/NewsAppUsingMVVM/Cell/ArticleTabeleViewModelCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTabeleViewModelCell.swift
+//  NewsAppUsingMVVM
+//
+//  Created by 효우 on 2022/10/18.
+//
+
+import UIKit
+
+class ArticleTabeleViewModelCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+}
--- NewsAppUsingMVVM/NewsAppUsingMVVM/Controllers/NewsListTableViewController.swift
+++ NewsAppUsingMVVM/NewsAppUsingMVVM/Controllers/NewsListTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -21,7 +23,34 @@ extension NewsListTableViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         guard let url = URL(string: AppKey.BASE_URL) else { return }
         WebService().getArticles(url: url) { _ in
-            
+            WebService().getArticles(url: url) { article in
+                guard let article = article else { return }
+                self.articleListVM = ArticleListViewModel(article: article)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        guard let numberOfSection = self.articleListVM?.numberOfsection else { return 0 }
+        return self.articleListVM == nil ? 0 : numberOfSection
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let count = self.articleListVM?.numberOfRowsSection(section) else { return 0 }
+        return count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTabeleViewModelCell", for: indexPath) as? ArticleTabeleViewModelCell else {
+            fatalError("Cell not Found")
+        }
+        let articleVM = self.articleListVM?.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM?.title
+        cell.descriptionLabel.text = articleVM?.description
+        return cell
     }
 }
