b959cacdf4f779ec7c2954ed18506ad68488e506
wooooozin <wooooozin@gmail.com>
2022-10-19 23:38:39 +0900

    프로젝트 생성 및 뷰 및 컨트롤러 생성

---
+++ CoffeeOrderProject/CoffeeOrderProject/Controllers/AddNewOrderController.swift
@@ -0,0 +1,12 @@
+//
+//  AddNewOrderController.swift
+//  CoffeeOrderProject
+//
+//  Created by 효우 on 2022/10/19.
+//
+
+import UIKit
+
+class AddNewOrderController: UIViewController {
+    
+}
---
+++ CoffeeOrderProject/CoffeeOrderProject/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  CoffeeOrderProject
+//
+//  Created by 효우 on 2022/10/19.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+    
+}
